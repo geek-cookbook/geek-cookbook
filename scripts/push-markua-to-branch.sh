068342a36554581ca57a4a2b9999bb2c48be3445
David Young <davidy@funkypenguin.co.nz>
2018-06-06 21:28:13 +1200

    Add Travis test 7

--- scripts/push-markua-to-branch.sh
+++ scripts/push-markua-to-branch.sh
@@ -7,15 +7,15 @@ setup_git() {
 
 commit_website_files() {
   git checkout -b leanpub-preview
-  git pull
   git add manuscript 
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
   #git remote add origin-leanpub-preview https://${LEANPUB-PREVIEW-TOKEN}@github.com/funkypenguin/geek-cookbook.git > /dev/null 2>&1
-  git remote add origin-leanpub-preview https://${TOKEN}@github.com/funkypenguin/geek-cookbook.git
-  git push --quiet --set-upstream origin-leanpub-preview leanpub-preview 
+  #git remote add origin-leanpub-preview https://${TOKEN}@github.com/funkypenguin/geek-cookbook.git
+  #git push --quiet --set-upstream origin-leanpub-preview leanpub-preview 
+  git push --quiet --set-upstream origin leanpub-preview 
 }
 
 setup_git
