26e9fa43325ec6794b83be10e60829f462928a56
David Young <davidy@funkypenguin.co.nz>
2018-06-06 21:48:01 +1200

    Add Travis test 11

--- scripts/markdown-to-markua.sh
+++ scripts/markdown-to-markua.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Markua doesn't know what to do with 4 backticks (````), so convert to 3:
-find manuscript -type f -name "*.md" -print0 | xargs -0 sed "s/````/```/g"
+find manuscript -type f -name "*.md" -print0 | xargs -0 sed -i "s/\`\`\`\`/\`\`\`/g"
 
 # Do nothing, yet. This is where the sed magic will go
