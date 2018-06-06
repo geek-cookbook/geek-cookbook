bd33ac6c595b66504fae70f6433267d5acd85ce5
David Young <davidy@funkypenguin.co.nz>
2018-06-06 22:26:35 +1200

    Remove Emojis

--- scripts/markdown-to-markua.sh
+++ scripts/markdown-to-markua.sh
@@ -3,4 +3,8 @@
 # Markua doesn't know what to do with 4 backticks (````), so convert to 3:
 find manuscript -type f -name "*.md" -print0 | xargs -0 sed -i "s/\`\`\`\`/\`\`\`/g"
 
+# Markua doesn't like emojis, so remove them:
+find manuscript -type f -name "*.md" -print0 | xargs -0 sed -i "s/👏//g"
+find manuscript -type f -name "*.md" -print0 | xargs -0 sed -i "s/💬//g"
+
 # Do nothing, yet. This is where the sed magic will go
