9829edd81a50611b3aac77cbe3aec96d770e32c4
Bencey <33912370+Bencey@users.noreply.github.com>
2018-06-12 09:14:01 +1200

    Turtlecoin replacement

--- scripts/markdown-to-markua.sh
+++ scripts/markdown-to-markua.sh
@@ -2,6 +2,7 @@
 
 # Markua doesn't know what to do with 4 backticks (````), so convert to 3:
 find manuscript -type f -name "*.md" -print0 | xargs -0 sed -i "s/\`\`\`\`/\`\`\`/g"
+find manuscript -type f -name "*.md" -print0 | xargs -0 sed -i "s/TRTLv2qCKYChMbU5sNkc85hzq2VcGpQidaowbnV2N6LAYrFNebMLepKKPrdif75x5hAizwfc1pX4gi5VsR9WQbjQgYcJm21zec4/https://geek-cookbook.funkypenguin.co.nz/support/"
 
 # Markua doesn't like emojis, so remove them:
 find manuscript -type f -name "*.md" -print0 | xargs -0 sed -i "s/👏//g"
@@ -21,7 +22,7 @@ find manuscript -type f -name "*.md" -print0 | xargs -0 sed -i "s/🎥//g"
 find manuscript -type f -name "*.md" -print0 | xargs -0 sed -i "s/🎵//g"
 find manuscript -type f -name "*.md" -print0 | xargs -0 sed -i "s/😁//g"
 
-# Thanks Bencey for this! (Bencey_#8587
+# Thanks Bencey for this! (Bencey_#8587)
 
 
 
