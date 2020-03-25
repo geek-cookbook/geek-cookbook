6fb8050d0afce1e755534f4801defb4b32228825
David Young <davidy@funkypenguin.co.nz>
2020-03-26 09:38:30 +1300

    Add some sexy new mkdocs plugins

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run --rm --name mkdocs-material -it -p 8000:8000 -v ${PWD}:/docs squidfunk/mkdocs-material
+docker build . -t funkypenguin/mkdocs-material
+docker run --rm --name mkdocs-material -it -p 8000:8000 -v ${PWD}:/docs funkypenguin/mkdocs-material
