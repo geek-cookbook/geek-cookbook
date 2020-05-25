81d50951bf8541a3e607b15518c41acabe9034df
David Young <davidy@funkypenguin.co.nz>
2020-05-25 15:14:02 +1200

    Modernize the ceph recipe

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,22 +4,22 @@
 # Copy the contents of "manuscript" to a new "publish" folder
 mkdir -p publish
 mkdir -p publish/overrides
-cp -r {manuscript,overrides} publish/ 
+cp -pr {manuscript,overrides} publish/ 
 cp mkdocs.yml publish/
 
-# Append a common footer to all recipes/swarm docs
-for i in `find publish/manuscript/ -name "*.md" | grep -v index.md`
-do
-	# Does this recipe already have a "tip your waiter" section?
-	grep -q "Tip your waiter" $i 
-	if [ $? -eq 1 ]
-	then
-		echo -e "\n" >> $i
-		cat scripts/recipe-footer.md >> $i 
-	else
-		echo "WARNING - hard-coded footer exists in $i"
-	fi
-done
+# # Append a common footer to all recipes/swarm docs
+# for i in `find publish/manuscript/ -name "*.md" | grep -v index.md`
+# do
+# 	# Does this recipe already have a "tip your waiter" section?
+# 	grep -q "Tip your waiter" $i 
+# 	if [ $? -eq 1 ]
+# 	then
+# 		echo -e "\n" >> $i
+# 		cat scripts/recipe-footer.md >> $i 
+# 	else
+# 		echo "WARNING - hard-coded footer exists in $i"
+# 	fi
+# done
 
 # Now build the docs for publishing
 mkdocs build -f publish/mkdocs.yml
