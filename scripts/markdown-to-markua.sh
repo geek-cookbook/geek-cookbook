29226480cea75ab4af1183f335658bfdcff37528
David Young <davidy@funkypenguin.co.nz>
2020-06-03 12:56:10 +1200

    Update markua emoji-stripping magic

--- scripts/markdown-to-markua.sh
+++ scripts/markdown-to-markua.sh
@@ -7,5 +7,8 @@ find manuscript -type f -name "*.md" -print0 | xargs -0 sed -i "s/\`\`\`\`/\`\`\
 find manuscript -type f -name "*.md" -print0 | xargs -0 sed -i "s/(\//(https:\/\/geek-cookbook.funkypenguin.co.nz\/)/g"
 
 # strip emojis
-$pattern = "[\x{1f300}-\x{1f5ff}\x{1f900}-\x{1f9ff}\x{1f600}-\x{1f64f}\x{1f680}-\x{1f6ff}\x{2600}-\x{26ff}\x{2700}-\x{27bf}\x{1f1e6}-\x{1f1ff}\x{1f191}-\x{1f251}\x{1f004}\x{1f0cf}\x{1f170}-\x{1f171}\x{1f17e}-\x{1f17f}\x{1f18e}\x{3030}\x{2b50}\x{2b55}\x{2934}-\x{2935}\x{2b05}-\x{2b07}\x{2b1b}-\x{2b1c}\x{3297}\x{3299}\x{303d}\x{00a9}\x{00ae}\x{2122}\x{23f3}\x{24c2}\x{23e9}-\x{23ef}\x{25b6}\x{23f8}-\x{23fa}]";
-find manuscript -type f -name "*.md" -print0 | xargs -0 sed -i "s/${pattern}//g"
+for file in `find manuscript -type f -name "*.md" -print0`
+do
+    tr -cd '\11\12\15\40-\176' < $file > $file-clean
+    mv $file-clean $file
+done
