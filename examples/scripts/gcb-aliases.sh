e71053825e2d3240ffae414a83e68ebd9504e758
Benjamin Durham <33912370+Bencey@users.noreply.github.com>
2020-06-21 17:50:41 -0700

    Some handy aliases to deploy and remove services :) (#119)

--- examples/scripts/gcb-aliases.sh
+++ examples/scripts/gcb-aliases.sh
@@ -14,3 +14,5 @@ alias dkideps='docker-image-dependencies'  # Output a graph of image dependencie
 alias dkre='docker-runtime-environment'  # List environmental variables of the supplied image ID
 alias dkelc='docker exec -it `dklcid` bash' # Enter last container (works with Docker 1.3 and above)
 alias git='docker run -v $PWD:/var/data -v /var/data/git-docker/data/.ssh:/root/.ssh funkypenguin/git-docker git' # Run git client in a container (for hosts witohut git)
+alias dsd='docker stack deploy "$1" -c /var/data/config/"$1"/"$1".yml
+alias dsr='docker stack rm "$1"
