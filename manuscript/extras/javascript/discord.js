da9b7ab5b82a8fcb7e2b114961a9190a9ff19363
David Young <davidy@funkypenguin.co.nz>
2021-01-06 23:12:50 +0000

    Update widgetbot embed code

--- manuscript/extras/javascript/discord.js
+++ manuscript/extras/javascript/discord.js
@@ -1,8 +1,7 @@
 <script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer>
   const button = new Crate({
     server: '396055506072109067',
-    channel: '456689991326760973',
-    shard: 'https://e.widgetbot.io',
+    channel: '396055506663374849',
     color: '#795548',
     indicator: false,
     notifications: true
