dca4d9964c1a8e896c5e0c864474c0398cfd957f
David Young <davidy@funkypenguin.co.nz>
2021-01-07 13:10:15 +1300

    Debugging...

--- manuscript/extras/javascript/discord.js
+++ manuscript/extras/javascript/discord.js
@@ -1,6 +1,4 @@
-<script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer>
-  new Crate({
-    server: '396055506072109067', // Funky Penguin's Geek Cookbook
-    channel: '396055506663374849' // #general
-  })
-</script>
+new Crate({
+  server: '396055506072109067', // Funky Penguin's Geek Cookbook
+  channel: '396055506663374849' // #general
+})
