46b21f6415aab6242e0b0736a88d40048c869c2b
David Young <davidy@funkypenguin.co.nz>
2021-01-07 13:42:29 +1300

    Tweak notification

--- manuscript/extras/javascript/discord.js
+++ manuscript/extras/javascript/discord.js
@@ -1,4 +1,4 @@
-const button = new Crate({
+new Crate({
   server: '396055506072109067',
   channel: '396055506663374849',
   color: 'black',
@@ -6,4 +6,9 @@ const button = new Crate({
   notifications: true
 })
 
-button.notify('Need a 🤚? Hot, sweaty geeks are waiting to chat to you! Click 👇')
+// Display for 2 seconds + custom avatar
+crate.notify({
+  content: 'Need a 🤚? Hot, sweaty geeks are waiting to chat to you! Click 👇',
+  timeout: 2000,
+  avatar: 'https://avatars2.githubusercontent.com/u/1524686?s=400&v=4'
+})
