1997b17bedb4f228868d9c1be45eb0a58eade85c
David Young <davidy@funkypenguin.co.nz>
2021-01-07 13:20:26 +1300

    Tweak color

--- manuscript/extras/javascript/discord.js
+++ manuscript/extras/javascript/discord.js
@@ -1,9 +1,9 @@
 const button = new Crate({
   server: '396055506072109067',
   channel: '396055506663374849',
-  color: '#795548',
+  color: '#black',
   indicator: false,
   notifications: true
 })
 
-button.notify('Need a 🤚? Hot sweaty geeks are waiting to chat to you! Click 👇')
+button.notify('Need a 🤚? Hot, sweaty geeks are waiting to chat to you! Click 👇')
