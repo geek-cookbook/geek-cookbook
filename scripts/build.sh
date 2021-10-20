1131ccb55e3fdd9c708598824699fd33459ad4b3
David Young <davidy@funkypenguin.co.nz>
2021-10-21 09:40:30 +1300

    Debug build process
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,10 +2,10 @@
 # This script prepares mkdocs for a build (there are some adjustments to be made to the recipes before publishing)
 
 # Fetch git history so that we get last-updated timestamps
-git fetch --unshallow
+# git fetch --unshallow
 
 # Run python build script to check for errors
-python3 scripts/build.py mkdocs.yml
+# python3 scripts/build.py mkdocs.yml
 
 # install mkdocs (or insiders version, if we're passed a GH_TOKEN var)
 if [ -z "$GH_TOKEN" ]
@@ -14,7 +14,7 @@ then
   mkdocs build -f mkdocs.yml
 else
   # Bypass search issue described at https://github.com/squidfunk/mkdocs-material/issues/3053
-  git clone --depth 1 https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material-insiders.git
+  # git clone --depth 1 https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material-insiders.git
   pip install -e mkdocs-material-insiders  
   mkdocs build -f mkdocs-insiders.yml
 fi
