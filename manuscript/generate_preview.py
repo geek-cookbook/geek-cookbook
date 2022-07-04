529371baf7a2a6558b0888a9cd75e7793cbaebd7
David Young <davidy@funkypenguin.co.nz>
2022-07-04 12:27:57 +1200

    Cleanup robots.txt

--- manuscript/generate_preview.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-with open("Book.txt") as f:
-    print ('echo "Starting build of {book}.epub";'
-            "pandoc {files} " +
-            "--table-of-contents --top-level-division=chapter -o {book}.epub;"
-            'echo "  {book}.epub created."'
-            ).format(book="Book", files=f.read().replace("\n", " "))
