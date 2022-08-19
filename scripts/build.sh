52858bfb19a6edba729af35fbcee6f56523b2178
David Young <davidy@funkypenguin.co.nz>
2022-08-19 18:36:59 +1200

    Add ENABLE_PDF_EXPORT
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,18 +7,16 @@
 # Run python build script to check for errors
 # python3 scripts/build.py mkdocs.yml
 
-ENABLE_PDF_EXPORT=1
-
 # install mkdocs (or insiders version, if we're passed a GH_TOKEN var)
 if [ -z "$GH_TOKEN" ]
 then
   pip install mkdocs-material
-  mkdocs build -f mkdocs.yml
+  ENABLE_PDF_EXPORT=1 mkdocs build -f mkdocs.yml
 else
   # Bypass search issue described at https://github.com/squidfunk/mkdocs-material/issues/3053
   # git clone --depth 1 https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material-insiders.git
   pip install -e mkdocs-material-insiders  
-  mkdocs build -f mkdocs-insiders.yml
+  ENABLE_PDF_EXPORT=1 mkdocs build -f mkdocs-insiders.yml
 fi
 
 # Setup any necessary netlify redirects
