0e527a8e36fbe39282dacb210b3c0542240d512f
David Young <davidy@funkypenguin.co.nz>
2022-08-19 18:21:15 +1200

    Another attempt
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,10 +2,10 @@
 # This script prepares mkdocs for a build (there are some adjustments to be made to the recipes before publishing)
 
 # Fetch git history so that we get last-updated timestamps
-git fetch --unshallow
+# git fetch --unshallow
 
 # Run python build script to check for errors
-python3 scripts/build.py mkdocs.yml
+# python3 scripts/build.py mkdocs.yml
 
 ENABLE_PDF_EXPORT=1
 
