05ea01ea72e5816e0993979b42078e69c5b44d11
David Young <davidy@funkypenguin.co.nz>
2023-03-25 13:01:32 +1300

    Avoid needing to build image locally
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- scripts/serve-insiders.sh
+++ scripts/serve-insiders.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # docker pull ghcr.io/geek-cookbook/mkdocs-material-insiders
-docker build --build-arg FROM_SOURCE=ghcr.io/geek-cookbook/mkdocs-material-insiders . -t funkypenguin/mkdocs-material
-docker run --rm --name mkdocs-material -it -p 8123:8000 -v ${PWD}:/docs -e PROD_BUILD=false funkypenguin/mkdocs-material serve \
+docker build --build-arg FROM_SOURCE=ghcr.io/geek-cookbook/mkdocs-material-insiders . -t funkypenguin/mkdocs-material-insiders
+docker run --rm --name mkdocs-material -it -p 8123:8000 -v ${PWD}:/docs -e PROD_BUILD=false funkypenguin/mkdocs-material-insiders serve \
     --dev-addr 0.0.0.0:8000 \
     --dirtyreload \
     --config-file mkdocs-insiders.yml
--- scripts/serve.sh
+++ scripts/serve.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker pull squidfunk/mkdocs-material:latest
-docker build . -t funkypenguin/mkdocs-material
-docker run --rm --name mkdocs-material -it -p 8123:8000 -v ${PWD}:/docs -e PROD_BUILD=false funkypenguin/mkdocs-material serve \
+# docker pull squidfunk/mkdocs-material:latest
+# docker build . -t funkypenguin/mkdocs-material
+docker run --rm --name mkdocs-material -it -p 8123:8000 -v ${PWD}:/docs -e PROD_BUILD=false ghcr.io/geek-cookbook/mkdocs-material:master serve \
     --dev-addr 0.0.0.0:8000 \
     --dirtyreload
