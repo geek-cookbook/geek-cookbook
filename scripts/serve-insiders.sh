ebec49f721760032d6ab4582c3843a72e7f02f72
David Young <davidy@funkypenguin.co.nz>
2023-05-15 15:57:42 +1200

    Un-break social plugin (I hope)
    Signed-off-by: David Young <davidy@funkypenguin.co.nz>

--- scripts/serve-insiders.sh
+++ scripts/serve-insiders.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# docker pull ghcr.io/geek-cookbook/mkdocs-material-insiders
+docker pull ghcr.io/geek-cookbook/mkdocs-material-insiders
 docker build --build-arg FROM_SOURCE=ghcr.io/geek-cookbook/mkdocs-material-insiders . -t funkypenguin/mkdocs-material-insiders
 docker run --rm --name mkdocs-material -it -p 8123:8000 -v ${PWD}:/docs -e PROD_BUILD=false funkypenguin/mkdocs-material-insiders serve \
     --dev-addr 0.0.0.0:8000 \
